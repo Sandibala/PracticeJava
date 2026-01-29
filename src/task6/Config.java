6ec85c9d2e29f1cc3f4e4f2efee9fa9bdaf64a04
Sandibala <sandibalaamze17@gmail.com>
2026-01-29 15:03:51 +0500

    Task6

---
+++ src/task6/Config.java
@@ -0,0 +1,33 @@
+package task6;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public class Config implements Serializable {
+    private static volatile Config instance;
+    private Config() {
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance()!");
+        }
+    }
+    public static Config getInstance() {
+        if (instance == null) {
+            synchronized (Config.class) {
+                if (instance == null) {
+                    instance = new Config();
+                }
+            }
+        }
+        return instance;
+    }
+
+    protected Object readResolve() throws ObjectStreamException {
+        return getInstance();
+    }
+
+    private String appName = "MyApp";
+
+    public String getAppName() {
+        return appName;
+    }
+}
